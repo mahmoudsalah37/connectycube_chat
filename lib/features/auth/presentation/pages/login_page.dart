59c99130c2dfb4645b863edad344ad356bd0030c
dev01 <ahmednasser74>
2021-08-26 07:10:01 +0200

    handle loading in image and voice sending, create logout dialog, remove same user of user list in users_page.dart

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,4 +1,6 @@
-import '../../domin/usecases/login_usecase.dart';
+import 'package:connectycube_chat/core/src/widgets/loading_indicator_widget.dart';
+import 'package:connectycube_chat/core/utils/validator.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../../../../core/src/colors.dart';
 import '../../../../core/src/routes.dart';
@@ -14,103 +16,87 @@ class LoginPage extends GetView<LoginController> {
   Widget build(BuildContext context) {
     final res = Responsive(context);
     ThemeData theme = Theme.of(context);
-
-    return Scaffold(
-      body: Form(
-        key: controller.formKey,
-        child: ListView(
-          padding: EdgeInsets.only(top: res.getHeight(10), left: 10, right: 10),
-          children: [
-            SizedBox(height: res.getHeight(2)),
-            Center(
-              child: Image.asset(
-                'assets/images/toast_icon.png',
-                height: res.getHeight(24),
-              ),
-            ),
-            SizedBox(height: res.getHeight(6)),
-            Column(
+    return Obx(
+      () => ModalProgressHUD(
+        inAsyncCall: controller.getLoadingIndicator,
+        progressIndicator: LoadingIndicatorWidget(),
+        child: Scaffold(
+          body: Form(
+            key: controller.formKey,
+            child: ListView(
+              padding:
+                  EdgeInsets.only(top: res.getHeight(10), left: 10, right: 10),
               children: [
-                TextFieldWidget(
-                  controller: controller.userNameTEC,
-                  hint: 'Login',
-                  inputType: TextInputType.text,
-                  prefixIcon: Icons.person_outline,
-                  validator: isEmpty,
-                ),
                 SizedBox(height: res.getHeight(2)),
-                TextFieldWidget(
-                  controller: controller.passwordTEC,
-                  hint: 'Password',
-                  inputType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  prefixIcon: Icons.lock_outline,
-                  validator: (v) {
-                    final value = v ?? '';
-                    if (value.isEmpty) {
-                      return 'أدخل قيمة';
-                    }
-                  },
+                Center(
+                  child: Image.asset(
+                    'assets/images/toast_icon.png',
+                    height: res.getHeight(24),
+                  ),
                 ),
-              ],
-            ),
-            CustomButton(
-              onPressed: () {},
-              title: 'Forget password?',
-              alignment: Alignment.centerLeft,
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: res.getWidth(20)),
-              child: CustomButton(
-                title: 'Login',
-                onPressed: () async {
-                  final isValid =
-                      controller.formKey.currentState?.validate() ?? false;
-                  if (isValid) {
-                    final userName = controller.userNameTEC.text;
-                    final password = controller.passwordTEC.text;
-                    final params =
-                        LoginParams(login: userName, password: password);
-                    controller.login(params: params);
-                  }
-                },
-              ),
-            ),
-            SizedBox(height: res.getHeight(16)),
-            CustomButton(
-              onPressed: () => Get.offNamed(Routes.registerPage),
-              title: 'Forget password?',
-              alignment: Alignment.center,
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              child: RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  text: 'don’t have an account?',
-                  style: TextStyle(color: Colors.black),
+                SizedBox(height: res.getHeight(6)),
+                Column(
                   children: [
-                    TextSpan(
-                      text: ' \nSignUp!',
-                      style: theme.textTheme.headline2!.copyWith(
-                          color: CustomColors.yellowDeepColor,
-                          fontWeight: FontWeight.w800),
+                    TextFieldWidget(
+                      controller: controller.userNameTEC,
+                      hint: 'Login',
+                      inputType: TextInputType.text,
+                      prefixIcon: Icons.person_outline,
+                      validator: CustomValidator.userNameValidation,
+                    ),
+                    SizedBox(height: res.getHeight(2)),
+                    TextFieldWidget(
+                      controller: controller.passwordTEC,
+                      hint: 'Password',
+                      inputType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      prefixIcon: Icons.lock_outline,
+                      validator: CustomValidator.passwordValidator,
                     ),
                   ],
                 ),
-              ),
+                CustomButton(
+                  onPressed: () {},
+                  title: 'Forget password?',
+                  alignment: Alignment.centerLeft,
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: res.getWidth(20)),
+                  child: CustomButton(
+                    title: 'Login',
+                    onPressed: controller.login2,
+                  ),
+                ),
+                SizedBox(height: res.getHeight(16)),
+                CustomButton(
+                  onPressed: () => Get.offNamed(Routes.registerPage),
+                  title: 'Forget password?',
+                  alignment: Alignment.center,
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: 'don’t have an account?',
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: ' \nSignUp!',
+                          style: theme.textTheme.headline2!.copyWith(
+                              color: CustomColors.yellowDeepColor,
+                              fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
-  }
-
-  String? isEmpty(v) {
-    final value = v ?? '';
-    if (value.isEmpty) {
-      return 'أدخل قيمة';
-    }
   }
 }
--- lib/features/chat/presentation/pages/chat_page.dart
+++ lib/features/chat/presentation/pages/chat_page.dart
@@ -1,4 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:connectycube_chat/core/src/widgets/loading_indicator_widget.dart';
+import 'package:connectycube_chat/core/utils/injection_container.dart';
+import 'package:connectycube_chat/features/auth/domin/usecases/get_cache_user_usecase.dart';
+import 'package:connectycube_chat/features/chat/presentation/widgets/no_message_widget.dart';
 import '../getx/channels_controller.dart';
 import '../getx/chat_controller.dart';
 import '../getx/voice_record_controller.dart';
@@ -14,67 +18,64 @@ class ChatPage extends GetView<ChatController> {
   @override
   Widget build(BuildContext context) {
     final channelController = Get.find<ChannelsController>();
-    return Obx(() => Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
-            title: ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: ClipOval(
-                child: channelController.getUser.avatar == null
-                    ? Container()
-                    : CachedNetworkImage(
-                        imageUrl: channelController.getUser.avatar!,
-                        height: 40,
-                        width: 40,
-                        fit: BoxFit.cover,
-                      ),
-              ),
-              title: Text(channelController.getUser.fullName.toString()),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        title: Row(
+          children: [
+            ClipOval(
+              child: channelController.getUser.avatar == null
+                  ? Container()
+                  : CachedNetworkImage(
+                      imageUrl: channelController.getUser.avatar!,
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+            ),
+            SizedBox(width: 10),
+            Text(channelController.getUser.fullName.toString()),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: controller.obx(
+              (messageList) {
+                final length = messageList?.length;
+                return ListView.builder(
+                  itemCount: length,
+                  reverse: true,
+                  itemBuilder: (_, index) {
+                    return MessageItemWidget(
+                      index: index,
+                      isMe: false,
+                      messagesList: messageList ?? [],
+                    );
+                  },
+                );
+              },
+              onEmpty: NoMessageWidget(),
+              onLoading: Center(child: CircularProgressIndicator()),
             ),
           ),
-          body: Column(
-            children: [
-              Expanded(
-                child: controller.getMessagesList.isNotEmpty
-                    ? ListView.builder(
-                        itemCount: controller.getMessagesList.length,
-                        reverse: true,
-                        itemBuilder: (_, index) {
-                          return MessageItemWidget(
-                            index: index,
-                            isMe: true,
-                            messagesList: controller.getMessagesList,
-                          );
-                        },
-                      )
-                    : Center(child: CircularProgressIndicator()),
-              ),
-              ChatBottomBarWidget(
-                onSendImage: () async => await controller.sendImageMessage(),
-                onSendVoiceRecord: () async {
-                  final path = await voiceRecordController.stopRecord();
-                  await voiceRecordController.disposeRecord();
-                  final voiceRecordMessage =
-                      await voiceRecordController.sendVoiceRecord(path);
-                  controller.addMessageToList(voiceRecordMessage);
-                  Get.back();
-                },
-                onPauseVoiceRecord: () async {
-                  await voiceRecordController.pauseRecord();
-                },
-                onStopVoiceRecord: () async {
-                  await voiceRecordController.stopRecord();
-                  Get.back();
-                },
-                onResumeVoiceRecord: () {},
-                onSendMessage: (text) async {
-                  await controller.sendStringMessage(text);
-                },
-                onStartVoiceRecord: voiceRecordController.startRecord,
-              )
-            ],
-          ),
-        ));
+          ChatBottomBarWidget(
+            onSendImage: () async => await controller.sendImageMessage(),
+            onSendVoiceRecord: controller.sendVoiceMessage,
+            onPauseVoiceRecord: voiceRecordController.pauseRecord,
+            onStopVoiceRecord: () async {
+              await voiceRecordController.stopRecord();
+              Get.back();
+            },
+            onResumeVoiceRecord: () {},
+            onSendMessage: (text) async =>
+                await controller.sendStringMessage(text),
+            onStartVoiceRecord: voiceRecordController.startRecord,
+          )
+        ],
+      ),
+    );
   }
 }
